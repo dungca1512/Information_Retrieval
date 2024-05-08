15d5d476ba35463cbc5a833586e4d4202c96c320
dungca4 <dungca4@fpt.com>
2024-05-08 16:59:39 +0700

    Add Word2Vec.scala

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,3 +1,21 @@
+import scala.io.Source
+
 object Test {
-  def getName(name: String): String = s"Hello, $name"
+
+  def loadVector(path: String): List[Map[String, List[Double]]] = {
+    val source = Source.fromFile(path, "UTF-8")
+    val lines = source.getLines.toList
+    val result = lines.map(line => {
+      val data = line.split(" ").toList
+      val token = data.head
+      val vector = data.tail.map(_.toDouble)
+      Map(
+        "vector" -> vector
+      )
+    })
+    result
+  }
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
 }
