3c008776b489bef8c168452274252a0947e27a51
dungca <dungca1512@gmail.com>
2024-06-04 22:11:13 +0700

    Update Test.scala

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,28 +1,37 @@
 import org.apache.spark.ml.feature.{RegexTokenizer, Word2Vec}
 import org.apache.spark.sql.SparkSession
+import scala.io.StdIn
 
 object Test {
 
-  def main(args: Array[String]): Unit = {
-
+  private def findNearistToken(path: String, token: String, num: Int): Unit = {
     val spark = SparkSession.builder()
       .appName("My Spark Application")
       .master("local[*]")
       .getOrCreate()
-
-    val path = "/home/dungca/Desktop/Information_Retrieval/json/data.json"
     val df = spark.read.option("multiline", value = true).json(path)
-
     val regexTokenizer = new RegexTokenizer().setInputCol("content").setOutputCol("tokens").setPattern("""[\s)(,.;-?"]+""")
-
     val ef = regexTokenizer.transform(df)
-
     val word2vec = new Word2Vec().setInputCol("tokens").setOutputCol("vector").setVectorSize(5).setMinCount(2)
-
     val model = word2vec.fit(ef)
+    model.findSynonyms(token, num).show()
+    spark.stop()
+  }
 
-    val ff = model.transform(ef)
-
-    model.findSynonyms("lãi", 15).show()
+  def main(args: Array[String]): Unit = {
+    print("Enter OS: ")
+    val os = StdIn.readLine()
+    var path = ""
+    if (os == "windows") {
+      path = "E:/IdeaProjects/Information_Retrieval/json/data.json"
+    }
+    else {
+      path = "/home/dungca/IdeaProjects/Information_Retrieval/json/data.json"
+    }
+    print("Enter token: ")
+    val token = StdIn.readLine()
+    print("Enter num: ")
+    val num = StdIn.readInt()
+    findNearistToken(path, token, num)
   }
 }
